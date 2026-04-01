8ec6b20f9ea9cc55e8e13154c4a86ec2ef82b0be
boris920308 <jhoonj0308@gmail.com>
2026-04-01 11:39:11 +0900

    feat. 멀티 쓰레드 환경에서 안전하게 싱글톤 구현하기 - [권장] static inner class 사용

--- lectures/GoF/src/singleton/Settings.java
+++ lectures/GoF/src/singleton/Settings.java
@@ -2,20 +2,13 @@ package singleton;
 
 public class Settings {
 
-    private static volatile Settings instance;
-
     private Settings() {}
 
-    public static Settings getInstance() {
-        if (instance == null) {
-            // instance가 없는 경우에만 synchronized 블럭을 타게 되므로 비용적으로 이득이 있다.
-            synchronized (Settings.class) {
-                if (instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
 
-        return instance;
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
     }
 }
