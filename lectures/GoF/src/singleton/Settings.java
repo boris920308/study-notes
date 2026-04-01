19fe8d07d931d1bb9884b23491618dc0d4044bb6
boris920308 <jhoonj0308@gmail.com>
2026-04-01 11:35:50 +0900

    feat. 멀티 쓰레드 환경에서 안전하게 싱글톤 구현하기 - double checked locking 설명 주석 추가

--- lectures/GoF/src/singleton/Settings.java
+++ lectures/GoF/src/singleton/Settings.java
@@ -8,6 +8,7 @@ public class Settings {
 
     public static Settings getInstance() {
         if (instance == null) {
+            // instance가 없는 경우에만 synchronized 블럭을 타게 되므로 비용적으로 이득이 있다.
             synchronized (Settings.class) {
                 if (instance == null) {
                     instance = new Settings();
