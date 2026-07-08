d630d8cb82b1860dc75c882b32adbf4ab17a511c
boris920308 <jhoonj0308@gmail.com>
2026-07-08 12:58:24 +0900

    feat. example2, complete 이후 Exception발생

--- RxJava/src/lesson2/Lesson2.java
+++ RxJava/src/lesson2/Lesson2.java
@@ -5,7 +5,7 @@ import io.reactivex.rxjava3.core.Observable;
 public class Lesson2 {
 
     public static void main(String[] args) {
-        onErrorTest();
+        example2();
     }
 
     private static void onErrorTest() {
@@ -21,6 +21,28 @@ public class Lesson2 {
         onErrorTest.subscribe(
                 System.out::println,
                 error -> System.out.println("error!")
+        );
+    }
+
+    private static void example1() {
+        Observable.just(10, 20, 30)
+                .subscribe(
+                        value -> System.out.println("받은 값: " + value),
+                        error -> System.out.println("에러 발생"),
+                        () -> System.out.println("모든 데이터 수신 완료")
+                );
+    }
+
+    private static void example2() {
+        Observable.create(emitter -> {
+            emitter.onNext("A");
+            emitter.onComplete();
+            emitter.onNext("B");
+            emitter.onError(new RuntimeException("Error"));
+        }).subscribe(
+                System.out::println,
+                e -> System.out.println("Error"),
+                () -> System.out.println("Complete")
         );
     }
 }
